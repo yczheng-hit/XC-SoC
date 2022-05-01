2e7b55f712b0c61b45de94993376f47a217a8a59
yczheng-hit <zyc1271661038@gmail.com>
2022-05-01 10:15:10 +0800

    update RTL code format

--- RTL/vga_rom.v
+++ RTL/vga_rom.v
@@ -1,10 +1,8 @@
-module vga_rom (
-	input clk,
-	input [11:0] addr,
-  input [6:0] font_type,
-	output reg rdata
-);
-reg [7:0] memory [0:127][0:143];
+module vga_rom (input clk,
+                input [11:0] addr,
+                input [6:0] font_type,
+                output reg rdata);
+    reg [7:0] memory [0:127][0:143];
     initial begin
         `ifdef SIMULATION
         $readmemh("./font/font.hex", memory);
@@ -12,8 +10,8 @@ reg [7:0] memory [0:127][0:143];
         $readmemh("D:/hit_sim_soft/arm-term/XC-SoC/simulation/font/font.hex",mem);
         `endif
     end
- 
+    
     always @(posedge clk) begin
-      rdata <= memory[font_type][addr[11:3]]&(1'b1 << addr[2:0] )?1'b1:1'b0;
+        rdata <= memory[font_type][addr[11:3]]&(1'b1 << addr[2:0])?1'b1:1'b0;
     end
 endmodule
