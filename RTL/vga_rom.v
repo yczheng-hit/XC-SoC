f780a08aa565fc00a17a3222c823ccb8cd3737d6
yczheng-hit <zyc1271661038@gmail.com>
2022-05-07 12:10:19 +0800

    FPGA TEST PASSED, update vga driver, xdc file, add clk IP core

--- RTL/vga_rom.v
+++ RTL/vga_rom.v
@@ -1,17 +1,19 @@
 module vga_rom (input clk,
-                input [11:0] addr,
+                input [10:0] addr,
                 input [6:0] font_type,
                 output reg rdata);
-    reg [7:0] memory [0:127][0:143];
+    // 16*16*96
+    reg [7:0] mem [24575:0];
     initial begin
         `ifdef SIMULATION
-        $readmemh("./font/font.hex", memory);
+        $readmemh("/mnt/d/hit_sim_soft/arm-term/XC-SoC/RTL/font/font.hex", mem);
         `else
-        $readmemh("D:/hit_sim_soft/arm-term/XC-SoC/simulation/font/font.hex",mem);
+        $readmemh("D:/hit_sim_soft/arm-term/XC-SoC/RTL/font/font.hex",mem);
         `endif
     end
-    
+    wire [7:0]font;
+    assign font = mem[{font_type,addr[10:3]}];
     always @(posedge clk) begin
-        rdata <= memory[font_type][addr[11:3]]&(1'b1 << addr[2:0])?1'b1:1'b0;
+        rdata <= font&(1'b1 << addr[2:0])?1'b1:1'b0;
     end
 endmodule
